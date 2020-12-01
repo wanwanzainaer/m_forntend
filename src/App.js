985ee88ee3a65b9e5028dd95025a5c0553a8db8b
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-30 23:22:19 -0800

    Add update place component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
 const App = () => {
   return (
     <Router>
@@ -16,9 +17,8 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Users} />
           <Route path="/:userId/places" component={UserPlaces} exact />
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
+          <Route path="/places/new" exact component={NewPlace} />
+          <Route path="/places/:placeId" component={UpdatePlace} />
           <Redirect to="/" exact />
         </Switch>
       </main>
